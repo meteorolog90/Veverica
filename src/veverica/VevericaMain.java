60f6be1b3d64e959043a93ec8b1a14bb6f146f68
meteorolog90 <meteorolog90@hotmail.com>
2020-01-02 13:17:47 +0100

    add Veverica project

--- src/veverica/VevericaMain.java
+++ src/veverica/VevericaMain.java
@@ -3,6 +3,13 @@ package veverica;
 public class VevericaMain {
 
     public static void main(String[] args) {
+        Veverica v1 = new Veverica("Chip", "braon");
+        
+        v1.savijLevuRuku();
+        v1.pridrzi("LESNIK");
+        v1.ispruziLevuRuku();
+        System.out.println(v1.getStaDrzi());
+        
         
 
     }
