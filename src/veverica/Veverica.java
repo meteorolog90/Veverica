46d0bc9caaddd03483c93b7bb06065e85231ea7e
meteorolog90 <meteorolog90@hotmail.com>
2020-01-02 12:42:12 +0100

    add Veverica project

--- src/veverica/Veverica.java
+++ src/veverica/Veverica.java
@@ -4,5 +4,25 @@ public class Veverica {
 
     private String ime;
     private String boja;
+    private Ruka levaRuka, desnaRuka;
+    private String staDrzi;
 
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public void setBoja(String boja) {
+        this.boja = boja;
+    }
+
+    public Veverica(String ime, String boja) {
+        this.setIme(ime);
+        this.setBoja(boja);
+        this.levaRuka = new Ruka();
+        this.desnaRuka = new Ruka();
+        this.staDrzi = "NISTA";
+    }
+    
+    
+    
 }
--- src/veverica/VevericaMain.java
+++ src/veverica/VevericaMain.java
@@ -3,6 +3,7 @@ package veverica;
 public class VevericaMain {
 
     public static void main(String[] args) {
+        
 
     }
 
